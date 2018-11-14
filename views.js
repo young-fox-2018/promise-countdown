8f1e09d69e3f95f56db8008ee8f8afa36d07e89f
taqiabdulaziz <svx.annexiv@gmail.com>
2018-11-14 10:47:34 +0700

    init commit

--- views.js
+++ views.js
@@ -5,7 +5,17 @@ const chalk = require("chalk");
 const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
+  let m = Math.floor(seconds / 60)
+  let s = seconds % 60
+ 
+  if (m < 10) {
+    m = `0${m}`
+  } 
+  if (s < 10) {
+    s = `0${s}`
+  }
+  
+  console.log(chalk.cyan(figlet.textSync(`${m}:${s}`)));
 };
 
 module.exports = { displayCountdown };
