133084fdb69e972b9dee5ac7cd438b3fdce99def
Mochamad Faishal Amir <faishalkings@gmail.com>
2018-11-14 16:33:15 +0700

    commit1

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const { timer } = require("./controllers");
 
 const main = () => {
   const seconds = parseInt(process.argv[2]) || 10;
-  // Your code here...
+  timer(seconds)
+  .then(data => {
+
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
 };
 
 main();
--- views.js
+++ views.js
@@ -5,7 +5,21 @@ const chalk = require("chalk");
 const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
+  if (seconds < 10) {
+    console.log(chalk.cyan(figlet.textSync(`00:0${seconds}`)));
+  }
+  else if (seconds < 60) {
+    console.log(chalk.cyan(figlet.textSync(`00:${seconds}`)));
+  }
+  else if (seconds >= 60) {
+    let menit = Math.floor(seconds/60)
+    let detik = seconds%60
+    if (detik < 10) {
+      console.log(chalk.cyan(figlet.textSync(`0${menit}:0${detik}`)));
+    } else {
+      console.log(chalk.cyan(figlet.textSync(`0${menit}:${detik}`)));
+    }
+  }
 };
 
 module.exports = { displayCountdown };
