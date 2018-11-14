13ba87c483f31aab0caf84f49bd2f291aa4af032
ranggakusuma <ranggavskusuma@gmail.com>
2018-11-14 11:12:53 +0700

    promise countdown done

--- controllers.js
+++ controllers.js
@@ -3,16 +3,28 @@ const { displayCountdown } = require("./views");
 const timer = seconds =>
   new Promise((resolve, reject) => {
     const now = Date.now();
+    seconds += 2
     const then = now + seconds * 1000;
 
-    displayCountdown(seconds - 1);
-    setInterval(() => {
+    let myInterval = setInterval(() => {
       const secondsLeft = Math.floor((then - Date.now()) / 1000);
-      if (secondsLeft < 0) {
-        clearInterval();
-        resolve();
+      if (secondsLeft <= 0) {
+        clearInterval(myInterval);
+        reject();
       } else {
-        displayCountdown(secondsLeft);
+        let detik = secondsLeft % 60
+        let menit = Math.floor(secondsLeft / 60)
+
+        if (menit < 10) {
+          menit = '0'+menit.toString()
+        }
+
+        if (detik < 10) {
+          detik = '0'+detik.toString()
+        }
+
+        displayCountdown(`${menit}:${detik}`);
+        // displayCountdown(`test`);
       }
     }, 1000);
   });
