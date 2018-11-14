25169697ac76b50fb26a7d61255235b6bf539e28
sendyakbar <msendyakbars@gmail.com>
2018-11-14 22:03:54 +0700

    promise countdown done

--- views.js
+++ views.js
@@ -6,6 +6,15 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+  let min = Math.floor(seconds / 60)
+  let sec = seconds % 60
+  if (min < 10) {
+    min = '0' + min
+  }
+  if (sec < 10) {
+    sec = '0' + sec
+  }
+  console.log(chalk.yellow(figlet.textSync(`${min}:${sec}`)));
 };
 
 module.exports = { displayCountdown };
