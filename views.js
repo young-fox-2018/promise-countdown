2b6bf103629bbbb4ef985c4fbdc2885221bef393
abedlubis <lubisabednego@gmail.com>
2018-11-14 16:12:51 +0700

    promise countdown finish

--- views.js
+++ views.js
@@ -2,10 +2,17 @@ const clear = require("clear");
 const figlet = require("figlet");
 const chalk = require("chalk");
 
-const displayCountdown = seconds => {
+const displayCountdown = (seconds) => {
   clear();
+  let menit = Math.floor(seconds/60)
+  let detik = seconds % 60
+  if(menit  < 10 || detik < 10) {
+    menit = `0${menit}`
+    detik = `0${detik}`
+  }
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
+  console.log(chalk.cyan(figlet.textSync(`${menit} : ${detik}`)))
+  
 };
 
 module.exports = { displayCountdown };
