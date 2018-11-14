e10922d7c83aef1dc6079c9f6dc0781782cb475a
anharfathoni <anharaf28@gmail.com>
2018-11-14 12:26:17 +0700

    countdown timer using promise

--- controllers.js
+++ controllers.js
@@ -6,6 +6,7 @@ const timer = seconds =>
     const then = now + seconds * 1000;
 
     displayCountdown(seconds - 1);
+    
     setInterval(() => {
       const secondsLeft = Math.floor((then - Date.now()) / 1000);
       if (secondsLeft < 0) {
@@ -20,3 +21,37 @@ const timer = seconds =>
 module.exports = {
   timer
 };
+
+
+
+// const timer = seconds => {
+//   setInterval(function(){
+
+//     let data = ""
+//     let menit = Math.floor(seconds / 60)
+//     let detik = seconds % 60
+//     let strmenit = ""
+//     let strdetik = ""
+
+//     if(menit < 10){
+//       strmenit = "0"+menit
+//     } else {
+//       strmenit = ""+menit
+//     }
+//     if(detik < 10){
+//       strdetik = "0"+detik
+//     } else {
+//       strdetik = ""+detik
+//     }
+    
+//     data = `                    ${strmenit} : ${strdetik}`
+//     displayCountdown(data)
+
+//     seconds -= 1
+
+//     if(seconds < 0){
+//       clearInterval(this)
+//     }
+
+//   },1000)
+// };
