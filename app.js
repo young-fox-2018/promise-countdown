39fdafac63b8ba615444ebd57c5dd439b699f3ff
imanuel vicky <imanuvi@imanuels-MBP.localdomain>
2018-11-14 14:47:15 +0700

    done

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const { timer } = require("./controllers");
 const main = () => {
   const seconds = parseInt(process.argv[2]) || 10;
   // Your code here...
+  timer(seconds)
 };
 
 main();
--- controllers.js
+++ controllers.js
@@ -12,7 +12,28 @@ const timer = seconds =>
         clearInterval();
         resolve();
       } else {
-        displayCountdown(secondsLeft);
+        if (secondsLeft > 60) {
+          if (secondsLeft / 60 > 10) {
+            displayCountdown(`${Math.floor(secondsLeft/60)}: ${secondsLeft % 60}`)
+            if (secondsLeft % 60 <= 10) {
+                displayCountdown(`${Math.floor(secondsLeft/60)}: 0${secondsLeft % 60}`)
+            } else {
+                 displayCountdown(`${Math.floor(secondsLeft/60)}: ${secondsLeft % 60}`)
+            }
+          } else {
+            if (secondsLeft % 60 <= 10) {
+                displayCountdown(`0${Math.floor(secondsLeft/60)}: 0${secondsLeft % 60}`)
+            } else {
+                displayCountdown(`0${Math.floor(secondsLeft/60)}: ${secondsLeft % 60}`)
+            }
+          }
+      } else {
+          if (secondsLeft >= 10) {
+             displayCountdown(`00:${secondsLeft}`)
+          } else {
+              displayCountdown(`00:0${secondsLeft}`)
+          }
+      }
       }
     }, 1000);
   });
