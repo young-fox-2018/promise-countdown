71f0c9253305ab425889594c6d1257eb26215089
Kevin Wijaya <Kevin@Macbooknyaa.localdomain>
2018-11-14 11:41:43 +0700

    clear interval

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const { timer } = require("./controllers");
+const clear = require("clear");
+const figlet = require("figlet");
+const chalk = require("chalk");
 
 const main = () => {
   const seconds = parseInt(process.argv[2]) || 10;
   // Your code here...
   timer(seconds)
+  .then(()=>{
+    console.log(chalk.cyan(figlet.textSync("Time's Up")));
+  })
 };
 
 main();
--- controllers.js
+++ controllers.js
@@ -6,10 +6,10 @@ const timer = seconds =>
     const then = now + seconds * 1000;
 
     displayCountdown(seconds - 1);
-    setInterval(() => {
+    let interval = setInterval(() => {
       const secondsLeft = Math.floor((then - Date.now()) / 1000);
       if (secondsLeft < 0) {
-        clearInterval();
+        clearInterval(interval);
         resolve();
       } else {
         displayCountdown(secondsLeft);
