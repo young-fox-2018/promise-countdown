ce491aa8ecc709269992637bd8f07dc7d5c442f3
fauzia <fauziaazia171097@gmail.com>
2018-11-14 18:00:17 +0700

    finish

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const { timer } = require("./controllers");
 
 const main = () => {
   const seconds = parseInt(process.argv[2]) || 10;
-  // Your code here...
-};
+  timer(seconds)
 
+    .then(()=> {
+      console.log()
+    })
+
+    .catch((err)=>{
+      console.log(err)
+  // Your code here...
+});
+}
 main();
