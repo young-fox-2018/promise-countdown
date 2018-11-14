5e7190c3fe2d402c66f66f161a8700a8269c495d
Sulistiyono <sulistiyono@Sulistio-MacBook-Pro.local>
2018-11-14 11:54:04 +0700

    promise countdown done

--- controllers.js
+++ controllers.js
@@ -4,7 +4,6 @@ const timer = seconds =>
   new Promise((resolve, reject) => {
     const now = Date.now();
     const then = now + seconds * 1000;
-
     displayCountdown(seconds - 1);
     setInterval(() => {
       const secondsLeft = Math.floor((then - Date.now()) / 1000);
@@ -12,7 +11,10 @@ const timer = seconds =>
         clearInterval();
         resolve();
       } else {
-        displayCountdown(secondsLeft);
+        let menit = Math.floor(secondsLeft / 60)
+        let detik = secondsLeft % 60
+        if (menit < 9 && detik < 10) displayCountdown(`0${menit} : 0${detik}`)
+        else if (menit < 9 && detik >= 10) displayCountdown(`0${menit} : ${detik}`)
       }
     }, 1000);
   });
